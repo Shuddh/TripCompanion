7d5ceb832d9c12761dc71606b28dfe88192fa6c8
Shuddh <jainshuddhatm@gmail.com>
2018-07-21 18:01:50 -0700

    Glyphicon font files - bootstrap css update

---
+++ src/main/java/com/travelCompanion/Utility/TCConstants.java
@@ -0,0 +1,9 @@
+package com.travelCompanion.Utility;
+
+import java.io.Serializable;
+
+public class TCConstants implements Serializable {
+
+    public static final String HOME_PAGE_VIEW = "homeView";
+    public static final String ERROR_PAGE_VIEW = "errorPageView";
+}
